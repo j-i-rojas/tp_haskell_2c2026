9ca92e234d3fe691fd57655396455a6704a671f0
j-i-rojas <juanirojas2005@gmail.com>
2026-09-04 19:08:08 -0300

    init: tp-haskell

---
+++ tp1-base/TP1.hs
@@ -0,0 +1,134 @@
+module TP1 where
+
+data Caja = Bombilla Bool | Nada
+              deriving Eq
+instance Show Caja where
+    show = showDeCaja
+
+showDeCaja :: Caja -> String
+showDeCaja (Bombilla True) = "💡"
+showDeCaja (Bombilla False) = "⚪️"
+showDeCaja (Nada) = "🛑"
+
+data Circuito = Caja     Caja
+              | Serie    Circuito Circuito
+              | Paralelo Caja Circuito Circuito Caja
+                  deriving Eq
+instance Show Circuito where
+    show = showDeCircuito
+
+showDeCircuito :: Circuito -> String
+showDeCircuito (Caja caja) = showDeCaja caja
+showDeCircuito (Serie circuitoInicial circuitoFinal) =
+  (showDeCircuito circuitoInicial) ++ "-" ++ (showDeCircuito circuitoFinal)
+showDeCircuito (Paralelo cajaEntrada circuitoIzquierdo circuitoDerecho cajaSalida) =
+  (showDeCaja cajaEntrada) ++
+  "{" ++ (showDeCircuito circuitoIzquierdo) ++ "}" ++
+  "{" ++ (showDeCircuito circuitoDerecho) ++ "}" ++
+  (showDeCaja cajaSalida)
+
+showDeCircuitoConEstructura :: Circuito -> String
+showDeCircuitoConEstructura (Caja caja) = showDeCaja caja
+showDeCircuitoConEstructura (Serie circuitoInicial circuitoFinal) = "(" ++
+  (showDeCircuitoConEstructura circuitoInicial) ++
+    "-" ++
+  (showDeCircuitoConEstructura circuitoFinal) ++ ")"
+showDeCircuitoConEstructura (Paralelo cajaEntrada circuitoIzquierdo circuitoDerecho cajaSalida) =
+  (showDeCaja cajaEntrada) ++
+  "{" ++ (showDeCircuitoConEstructura circuitoIzquierdo) ++ "}" ++
+  "{" ++ (showDeCircuitoConEstructura circuitoDerecho) ++ "}" ++
+  (showDeCaja cajaSalida)
+
+on  = Bombilla True
+off = Bombilla False
+
+cajaOn   = Caja on
+cajaOff  = Caja off
+cajaNada = Caja Nada
+
+-- 1: recCircuito
+recCircuito ::
+    (Caja -> b) ->
+    (Circuito -> b -> Circuito -> b -> b) ->
+    (Caja -> Circuito -> b -> Circuito -> b -> Caja -> b) ->
+    Circuito ->
+    b
+recCircuito cCaja cSerie cParalelo c =
+    case c of
+        Caja caja -> cCaja caja
+        Serie circuitoInicial circuitoFinal -> cSerie circuitoInicial (rec circuitoInicial) circuitoFinal (rec circuitoFinal)
+        Paralelo cajaEntrada circuitoIzquierdo circuitoDerecho cajaSalida -> cParalelo cajaEntrada circuitoIzquierdo (rec circuitoIzquierdo) circuitoDerecho (rec circuitoDerecho) cajaSalida
+    where
+        rec = recCircuito cCaja cSerie cParalelo
+
+-- 2: foldCircuito
+
+foldCircuito ::
+    (Caja -> b) ->
+    (b -> b -> b) ->
+    (Caja -> b -> b -> Caja -> b) ->
+    Circuito ->
+    b
+foldCircuito cCaja cSerie cParalelo =
+    recCircuito
+        cCaja
+        (\_ circuitoInicial _ circuitoFinal -> cSerie circuitoInicial circuitoFinal)
+        (\cajaEntrada _ circuitoIzquierdo _ circuitoDerecho cajaSalida -> cParalelo cajaEntrada circuitoIzquierdo circuitoDerecho cajaSalida)
+
+-- 3 invertido
+
+invertido = undefined -- TODO: COMPLETAR
+
+-- 4: hayCaminoIluminado
+
+hayCaminoIluminado = undefined -- TODO: COMPLETAR
+
+-- 5: cantidadPrendidas
+
+cantidadPrendidas = undefined -- TODO: COMPLETAR
+
+-- 6: cajasDeCircuito
+
+cajasDeCircuito = undefined -- TODO: COMPLETAR
+
+-- 7: esCircuitoProlijo
+
+esCircuitoProlijo = undefined -- TODO: COMPLETAR
+
+-- 8: circuitoEmprolijado
+
+circuitoEmprolijado = undefined -- TODO: COMPLETAR
+
+-- 9: tienenLaMismaEstructura
+
+tienenLaMismaEstructura = undefined -- TODO: COMPLETAR
+
+-- 10: subCircuitoMásResistente
+
+subCircuitoMásResistente = undefined -- TODO: COMPLETAR
+
+{-- 11: Demostrar: alternado . alternado = id
+
+alternado :: Circuito -> Circuito
+{AC} alternado (Caja caja) = Caja (cajaAlternada caja)
+{AS} alternado (Serie ci cf) = Serie (alternado ci) (alternado cf)
+{AP} alternado (Paralelo ce ci cd cs) =
+       Paralelo (cajaAlternada ce) (alternado ci) (alternado cd) (cajaAlternada cs)
+
+cajaAlternada :: Caja -> Caja
+{CAN} cajaAlternada Nada = Nada
+{CAB} cajaAlternada Bombilla booleano = Bombilla not booleano
+
+(.) :: (b -> c) -> (a -> b) -> a -> c
+{C} (f . f) x = f (f x)
+
+id :: a -> a
+{I} id x = x
+
+not :: Bool -> Bool
+{NT} not True = False
+{NF} not False = True
+
+-- TODO: COMPLETAR
+
+--}
